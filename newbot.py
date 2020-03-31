ff55c58b4a17166d216a7af41e474a048dd9b688
Jack Kurtz <jackkennakurtz@gmail.com>
2020-03-30 20:17:20 -0700

    Got the extra on_message() to F off

--- newbot.py
+++ newbot.py
@@ -46,6 +46,5 @@ class MyClient(discord.Client):
 
         if message.content == '!motherrussia':
             await message.channel.send('Russia is our sacred power, Russia is our beloved country. Mighty will, great glory - Your possession for all time! Glory to our free Fatherland, The fraternal peoples of the age-old union, The ancestors of this wisdom of the people! Glory to the country! We proud of you! From the southern seas to the polar region Spread our forests and fields. You alone in the world! You are the only one - God?s native land! Glory to our free Fatherland, The fraternal peoples of the age-old union, The ancestors of this wisdom of the people! Glory to the country! We proud of you! Wide scope for dreams and for life The coming years open to us. Our loyalty to the Fatherland gives us strength. So it was, it is, and it always will be! Glory to our free Fatherland, The fraternal peoples of the age-old union, The ancestors of this wisdom of the people! Glory to the country! We proud of you!')
-    async def on_message()
 client = MyClient()
 client.run(TOKEN)
