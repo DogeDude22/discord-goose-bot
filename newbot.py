a192fa29f79a39da936bf47ffcd5491eb929979d
Jack Kurtz <jackkennakurtz@gmail.com>
2020-03-31 20:12:08 -0700

    Convert Commit 3
    Added response command

--- newbot.py
+++ newbot.py
@@ -17,6 +17,11 @@ hjonks = [
     'Honk'
     ]
 
+emojis = [
+    '<:takebell:694682279087439995>',
+    '<:goosebadge:694682278613221397>'
+]
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -28,7 +33,7 @@ async def on_ready():
     print('Logged on!')
 
 @bot.event
-async def on_member_join(self,member):
+async def on_member_join(self, member):
     await message.channel.send('Hi' + {member.name} + ', welcome to the soviet union!')
 
 @bot.command()
@@ -39,12 +44,14 @@ async def honk(ctx):
     #Say the line
     await ctx.send(random.choice(hjonks))
  
-        
-#async def on_message(self,message):
-    #don't respond to ourselves
- #   if message.author == self.user:
-  #      return
+@bot.command()
+async def respond(ctx):
+    '''
+    I like my custom emojis
+    '''
+    #Do the response
+    await ctx.message.add_reaction(random.choice(emojis))
+    
 
-   # if message.content == '!motherrussia':
-    #    await message.channel.send('Russia is our sacred power, Russia is our beloved country. Mighty will, great glory - Your possession for all time! Glory to our free Fatherland, The fraternal peoples of the age-old union, The ancestors of this wisdom of the people! Glory to the country! We proud of you! From the southern seas to the polar region Spread our forests and fields. You alone in the world! You are the only one - God?s native land! Glory to our free Fatherland, The fraternal peoples of the age-old union, The ancestors of this wisdom of the people! Glory to the country! We proud of you! Wide scope for dreams and for life The coming years open to us. Our loyalty to the Fatherland gives us strength. So it was, it is, and it always will be! Glory to our free Fatherland, The fraternal peoples of the age-old union, The ancestors of this wisdom of the people! Glory to the country! We proud of you!')
+        
 bot.run(TOKEN)
