578debca3398ac9c6de3bdd9bc1bd370ae24d7af
Jack Kurtz <jackkennakurtz@gmail.com>
2020-04-04 16:34:09 -0700

    Moved lists to new module

--- newbot.py
+++ newbot.py
@@ -8,35 +8,7 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import discord
 import builtins
-
-#List of all of our honks
-hjonks = [
-    'HONK',
-    'honk',
-    'hjonk',
-    'HJONK',
-    'hONk',
-    'HonK',
-    'hONK',
-    'HoNk',
-    'hOnK',
-    'Honk'
-    ]
-
-#A list of our emojis
-emojis = [
-    '<:takebell:694682279087439995>',
-    '<:goosebadge:694682278613221397>'
-]
-
-dadjokeslist = [
-    'Today, my son asked "Can I have a book mark?" and I burst into tears. 11 years old and he still doesnt know my name is Brian.',
-    'My wife is really mad at the fact that I have no sense of direction. So I packed up my stuff and right.',
-    'I bought some shoes from a drug dealer. I dont know what he laced them with, but I was tripping all day!',
-    'Did you know the first French fries werent actually cooked in France? They were cooked in Greece.',
-    'If a child refuses to sleep during nap time, are they guilty of resisting a rest?',
-    'The secret service isnt allowed to yell '"Get down!"' anymore when the president is about to be attacked. Now they have to yell "Donald, duck!'
-]
+import varibles
 
 #Get our bots token from a file
 load_dotenv()
@@ -65,7 +37,7 @@ async def honk(ctx):
     if toggle_commands.honktoggle == False:
         return
     else:
-        await ctx.send(random.choice(hjonks))
+        await ctx.send(random.choice(varibles.hjonks))
  
 @bot.command()
 async def respond(ctx):
@@ -76,7 +48,7 @@ async def respond(ctx):
     if toggle_commands.responsetoggle == False:
         return
     else:
-        await ctx.message.add_reaction(random.choice(emojis))
+        await ctx.message.add_reaction(random.choice(varibles.emojis))
 
 @bot.command()
 async def MeMe(ctx):
@@ -93,9 +65,9 @@ async def dadjoke(ctx):
     '''
     I'm not broken, I'm dad
     '''
-    if toggle_commands.dadtoggle
+    if toggle_commands.dadtoggle == False:
         return
     else:
-        await ctx.send(random.choice(dadjokeslist))
+        await ctx.send(random.choice(varibles.dadlist))
 
 bot.run(TOKEN)
---
+++ varibles.py
@@ -0,0 +1,27 @@
+hjonks = [
+    'HONK',
+    'honk',
+    'hjonk',
+    'HJONK',
+    'hONk',
+    'HonK',
+    'hONK',
+    'HoNk',
+    'hOnK',
+    'Honk'
+    ]
+
+#A list of our emojis
+emojis = [
+    '<:takebell:694682279087439995>',
+    '<:goosebadge:694682278613221397>'
+]
+
+dadlist = [
+    'Today, my son asked "Can I have a book mark?" and I burst into tears. 11 years old and he still doesnt know my name is Brian.',
+    'My wife is really mad at the fact that I have no sense of direction. So I packed up my stuff and right.',
+    'I bought some shoes from a drug dealer. I dont know what he laced them with, but I was tripping all day!',
+    'Did you know the first French fries werent actually cooked in France? They were cooked in Greece.',
+    'If a child refuses to sleep during nap time, are they guilty of resisting a rest?',
+    'The secret service isnt allowed to yell '"Get down!"' anymore when the president is about to be attacked. Now they have to yell "Donald, duck!'
+]
